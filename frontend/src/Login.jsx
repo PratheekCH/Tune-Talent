c1edffe2063ed26bcae5c8d01518d99a3e70f4bc
Sri Lakshmi Sivani Dadi <dadisrilakshmisivani@gmail.com>
2026-06-17 08:51:25 +0530

    Login

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./Login.css";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div>
+      <h2>Login</h2>
+
+      <form>
+        <input type="text" placeholder="Enter Username"/>
+        <input type="password" placeholder="Enter your password"/>
+        <button type="submit"> Login</button>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
